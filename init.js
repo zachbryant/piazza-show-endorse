b201148908463171fe56231b0e6383e92f07ef65
zachbryant <zbryant832@gmail.com>
2020-02-12 18:05:40 -0500

    save all

---
+++ init.js
@@ -0,0 +1,87 @@
+"use strict";
+
+function addJankIntoDom() {
+  var script = document.createElement("script");
+  script.appendChild(document.createTextNode("(" + interval + ")();"));
+  (document.body || document.head || document.documentElement).appendChild(
+    script
+  );
+}
+addJankIntoDom();
+
+/*window.oldUrlParams = new URLSearchParams(window.location.search);
+window.oldCid = oldUrlParams.get("cid");*/
+
+function interval() {
+  setInterval(function() {
+    if (!window.hasOwnProperty("P")) {
+      console.error("P is undefined!");
+      return;
+    }
+    var endorsements = [
+      {
+        sel: ".good_note",
+        class: "good_note",
+        content: P.note_view.content["tag_good"]
+      }
+    ];
+    if (P.answer.answers.hasOwnProperty("s_answer")) {
+      endorsements.push({
+        sel: "#member_answer .good_answer",
+        class: "good_answer",
+        content: P.answer.answers.s_answer["tag_endorse"]
+      });
+    }
+    if (P.answer.answers.hasOwnProperty("i_answer")) {
+      endorsements.push({
+        sel: "#instructor_answer .good_answer",
+        class: "good_answer",
+        content: P.answer.answers.i_answer["tag_endorse"]
+      });
+    }
+    var topFollowupContent = P.clarifying_discussion.followups;
+    var topFollowupContentKeys = Object.keys(topFollowupContent);
+    var followupContent = new Map();
+    topFollowupContentKeys.forEach(function(k, ind) {
+      var f = topFollowupContent[k];
+      followupContent.set(k, f);
+      if (f.children) {
+        for (child of f.children) {
+          followupContent.set(child.id, child);
+        }
+      }
+    });
+
+    var followups = document.querySelectorAll(".followup_actions");
+    followups.forEach(function(f, ind) {
+      var parent = f.parentElement;
+      var fId = parent.id;
+      var ob = {
+        sel: `#${fId}>.followup_actions span`,
+        class: "number",
+        content: followupContent.get(fId)["tag_good"]
+      };
+      endorsements.push(ob);
+    });
+
+    endorsements.forEach(e => {
+      if (e.content === undefined || e.content.length == 0) return;
+      $(e.sel).attr({
+        style:
+          "font-weight: bold !important; text-decoration: underline !important;",
+        tutorial: e.content
+          .map(u => {
+            if (u.admin) return `${u.name} (${u.role.toUpperCase()})`;
+            return `${u.name}`;
+          })
+          .join(", "),
+        class: `${e.class} btn-mini post_actions_number post_action main show-tipsy`
+      });
+      $(`.show-tipsy.${e.class}`).tipsy({
+        gravity: "n",
+        html: true,
+        title: "tutorial"
+      });
+    });
+  }, 500); // check every half second
+}
